ddc5dcf61e718ca5b79ca6357ebd047cb65e357e
pgm-yorbenbogaert <yorben.bogaert@student.arteveldehs.be>
2025-11-19 15:11:04 +0100

    Explained scope

---
+++ week-1/scope.js
@@ -0,0 +1,17 @@
+const message = 'Hey daar'
+
+function sayHello() {
+    const message = 'Bye!'
+
+
+   const say = function() {
+    
+    const newMessage = 'YOLO'
+    console.log(message)
+   }
+
+}
+
+sayHello()
+
+console.log(Newmessage)
