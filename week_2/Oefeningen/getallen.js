a06d28adbd305ddb1f3089c275036aa321d60c7a
pgm-yorbenbogaert <yorben.bogaert@student.arteveldehs.be>
2025-11-27 12:17:08 +0100

    oefeningen

--- week_2/Oefeningen/getallen.js
+++ week_2/Oefeningen/getallen.js
@@ -1,5 +1,22 @@
 const willekeurigeGetallen = [23, 7, 45, 12, 87, 3, 56, 19, 34, 91];
 
-const getallenGroterTien = willekeurigeGetallen.filter(willekeurigGetal => willekeurigGetal < 10)
 
-console.log(getallenGroterTien)
+const getallenGroterTien = willekeurigeGetallen.filter(willekeurigGetal => willekeurigGetal < 10);
+
+console.log(getallenGroterTien);
+
+const deleteLast = willekeurigeGetallen.pop();
+
+console.log(willekeurigeGetallen)
+
+const deleteFirst = willekeurigeGetallen.shift();
+
+console.log(willekeurigeGetallen);
+
+const addFive = willekeurigeGetallen.push(5); 
+
+console.log(willekeurigeGetallen);
+
+const getalGroterTwintig = willekeurigeGetallen.every(willekeurigGetal => willekeurigGetal < 20);
+
+console.log(getalGroterTwintig);
